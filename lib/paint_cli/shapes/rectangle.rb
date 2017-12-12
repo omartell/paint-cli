6648243ae93a594adfba723d248dbf6935bdfc10
Oliver Martell <oliver.martell@gmail.com>
2017-12-12 17:44:54 +0000

    Paint rectangles

--- lib/paint_cli/shapes/rectangle.rb
+++ lib/paint_cli/shapes/rectangle.rb
@@ -1,4 +1,6 @@
 class PaintCli::Shapes::Rectangle < OpenStruct
+  attr_reader :x1, :y1, :x2, :y2
+
   def self.from_input(input)
     self.new(
       x1: Integer(input[0]),
@@ -8,5 +10,20 @@ class PaintCli::Shapes::Rectangle < OpenStruct
     )
   rescue ArgumentError, TypeError
     PaintCli::InvalidInput.new(input: input)
+  end
+
+  def initialize(x1:, y1:, x2:, y2:)
+    @x1, @y1, @x2, @y2 = x1, y1, x2, y2
+  end
+
+  def pixels
+    PaintCli::Shapes::Line.new(x1: x1, y1: y1, x2: x1, y2: y2).pixels
+      .merge(
+      PaintCli::Shapes::Line.new(x1: x1, y1: y1, x2: x2, y2: y1).pixels
+    ).merge(
+      PaintCli::Shapes::Line.new(x1: x2, y1: y2, x2: x2, y2: y1).pixels
+    ).merge(
+      PaintCli::Shapes::Line.new(x1: x2, y1: y2, x2: x1, y2: y2).pixels
+    )
   end
 end
--- spec/paint_cli/shapes/rectangle_spec.rb
+++ spec/paint_cli/shapes/rectangle_spec.rb
@@ -18,4 +18,17 @@ RSpec.describe PaintCli::Shapes::Rectangle do
       end
     end
   end
+
+  it "returns the pixels for the given coordinates" do
+    expect(described_class.new(x1: 1, y1: 1, x2: 3, y2: 3).pixels).to eq(
+      [1, 1] => "x",
+      [1, 2] => "x",
+      [1, 3] => "x",
+      [2, 1] => "x",
+      [2, 3] => "x",
+      [3, 1] => "x",
+      [3, 2] => "x",
+      [3, 3] => "x"
+    )
+  end
 end
